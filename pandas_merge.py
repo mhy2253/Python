5503416a184fe41fadc31fc340d7f711c957a738
Carrot <mhy2253@outlook.com>
2025-02-24 21:56:25 +0800

    更新 pandas_merge.py

--- pandas_merge.py
+++ pandas_merge.py
@@ -1,2 +1,6 @@
 import pandas as pd
-pd.read_excel('/path/data_1.xlsx', sheet_name='sheet1')
+df_1 = pd.read_excel(r'C:\Users\carrot\OneDrive\文档\Python\data_1.xlsx', sheet_name='sheet1')
+df_2 = pd.read_excel(r'C:\Users\carrot\OneDrive\文档\Python\data_2.xlsx', sheet_name='sheet1')
+df_3 = pd.merge(df_1,df_2,how='inner',on=['valu','valu2'])
+print(df_3.head())
+df_3.to_excel('./output.xlsx',index=False)
